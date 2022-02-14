8f9b8d10c9052c31655d7d3dedce8b1d2bcb4e61
gfszr <99412607+gfszr@users.noreply.github.com>
2022-02-14 16:55:26 +0200

    Add a simple symllink setup script that links conf files to this repo

---
+++ setup.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+CONFIG_DIR=$HOME/.config
+NVIM_DIR=$CONFIG_DIR/nvim
+TMUX_DIR=$HOME/.tmux
+TMUX_CONF=$HOME/.tmux.conf
+NVIM_CONF=$NVIM_DIR/init.vim
+VIM_CONF=$HOME/.vimrc
+ZSHRC=$HOME/.zshrc
+
+echo 'Checking for pre-existing files and directories'
+
+function run_command() {
+    echo $1
+    eval $1
+}
+
+function prompt_user() {
+    while true; do
+        read -p "$1 Continue? (Y/N)" yn
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+
+}
+
+function remove_pre_existing() {
+    if [ -e $1 ]; then
+        prompt_user "Removing pre-existing $1!"
+        run_command "rm -rf $1"
+    fi
+}
+
+function link() {
+    echo 'Symlinking' $2 ' -> ' $1
+    run_command "ln -s $1 $2"
+}
+
+
+remove_pre_existing $VIM_CONF
+remove_pre_existing $NVIM_DIR
+remove_pre_existing $TMUX_DIR
+remove_pre_existing $TMUX_CONF
+remove_pre_existing $ZSHRC
+
+link `pwd`/zshrc $ZSHRC
+echo 'zshrc file installed successfully!'
+
+link `pwd`/nvim $NVIM_DIR
+echo 'Nvim directory installed successfully!'
+
+link `pwd`/tmux $TMUX_DIR 
+echo 'Tmux directory installed successfully!'
+
+link `pwd`/tmux.conf $TMUX_CONF
+echo 'Tmux conf installed successfully!'
+
+link `pwd`/nvim/init.vim $VIM_CONF
+echo 'Vim .vimrc compatability symlink installed successfully!'
+
+
